5bfef7b8076cd7c63a5ea253bfb002d93cd8f108
Nikita Volsky <nikitoriy@gmail.com>
2023-02-22 18:19:27 +0400

    some more JSX and map function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import './App.css';
 
-const welcome = {
-  greeting: 'Wazzup',
-  title: 'React',
-};
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+];
 
-function getTitle(welcome) {
-  return welcome.title;
-};
 
 function App() {
   return (
     <div className="App">
       <h1>
-        {welcome.greeting} {getTitle(welcome)}!
+        My Hacker Stories
       </h1>
 
       <label htmlFor='search'>Search: </label>
       <input id='search' type='text' />
 
+      <hr />
+      <ul>
+        {list.map(function (item) {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title} </a>
+              </span>
+              <span>{item.author} </span>
+              <span>{item.num_comments} </span>
+              <span>{item.points}</span>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
